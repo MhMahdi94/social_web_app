8cb4f7c846e3ce124af58207de13d324228f50e7
MhMahdi94 <mohdmahdy94@gmail.com>
2023-06-06 20:27:03 +0400

    register page ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
 import Login from './pages/login/Login';
-
+import Register from './pages/register/Register';
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Register />
     </div>
   );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import './register.scss'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register">
+        <div className="card">
+            <div className="left">
+                <h1>Social App.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis interdum nulla at tincidunt. Mauris eget lectus venenatis, tristique metus vel, ornare elit. Donec quis turpis a magna egestas euismod. Maecenas in bibendum mi. Vestibulum sit amet nisl sodales, maximus lacus eu, bibendum turpis. Donec facilisis fermentum commodo. Etiam eu nulla tortor.</p>
+                <span>Do you have an account?</span>
+                <button>Login</button>
+            </div>
+            <div className="right">
+                <h1>Register</h1>
+                <form >
+                    <input type="text" placeholder="Username" />
+                    <input type="email" placeholder="Email"/>
+                    <input type="text" placeholder="name" />
+                    <input type="password" placeholder="Password"/>
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
