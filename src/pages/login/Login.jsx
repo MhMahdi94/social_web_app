245ea878b42ab4f3478aacf451a53fed7c9a3906
MhMahdi94 <mohdmahdy94@gmail.com>
2023-06-06 20:20:32 +0400

    login page ui

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import './login.scss'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+        <div className="card">
+            <div className="left">
+                <h1>Hello World.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis interdum nulla at tincidunt. Mauris eget lectus venenatis, tristique metus vel, ornare elit. Donec quis turpis a magna egestas euismod. Maecenas in bibendum mi. Vestibulum sit amet nisl sodales, maximus lacus eu, bibendum turpis. Donec facilisis fermentum commodo. Etiam eu nulla tortor.</p>
+                <span>Don't you have an account?</span>
+                <button>Register</button>
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form >
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password"/>
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
