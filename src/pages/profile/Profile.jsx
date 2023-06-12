6e058ecf745356dc7df277c2b0ed2f675621fe47
MhMahdi94 <mohdmahdy94@gmail.com>
2023-06-12 16:50:49 +0400

    profile page  ui

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
-
+import  './profile.scss'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import PlaceIcon from '@mui/icons-material/Place';
+import LanguageIcon from '@mui/icons-material/Language';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Posts from '../../components/posts/Posts';
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div style={{flex:6}} className='profile'>
+      <div className="images">
+        <img src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className='cover'/>
+        <img src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" className='profilePic'/>
+      </div>
+
+
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+              <a href="http://facebook.com">
+                <FacebookOutlinedIcon fontSize="large" />
+              </a>
+              <a href="http://facebook.com">
+                <InstagramIcon fontSize="large" />
+              </a>
+              <a href="http://facebook.com">
+                <TwitterIcon fontSize="large" />
+              </a>
+              <a href="http://facebook.com">
+                <LinkedInIcon fontSize="large" />
+              </a>
+              <a href="http://facebook.com">
+                <PinterestIcon fontSize="large" />
+              </a>
+          </div>
+          <div className="center">
+            <span>John Doe</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon/>
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon/>
+                <span>socialapp.com</span>
+              </div>
+              
+            </div>
+          </div>
+          <div className="right">
+            <MailOutlineIcon  />
+            <MoreVertIcon  />
+          </div>
+          
+        </div>
+        <Posts/>
+      </div>
+    </div>
   )
 }
 
